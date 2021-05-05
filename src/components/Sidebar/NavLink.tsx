ceae60e5a1d157eae92439227602a59d3a9e8bdd
Rellyso <rellysondouglas2015@gmail.com>
2021-05-04 22:18:01 -0300

    added sidebarNav, NavSection and Navlink

---
+++ src/components/Sidebar/NavLink.tsx
@@ -0,0 +1,18 @@
+import { LinkProps as ChakraLinkProps } from "@chakra-ui/react";
+import Icon from "@chakra-ui/icon";
+import { Link, Text } from "@chakra-ui/layout";
+import { ElementType } from "react";
+
+interface NavLinkProps extends ChakraLinkProps {
+  icon: ElementType;
+  children: string;
+}
+
+export function NavLink({ icon, children, ...rest }: NavLinkProps) {
+  return (
+    <Link display="flex" align="center" {...rest}>
+      <Icon as={icon} fontSize="20" />
+      <Text ml="4" fontWeight="bold">{children}</Text>
+    </Link>
+  )
+}
