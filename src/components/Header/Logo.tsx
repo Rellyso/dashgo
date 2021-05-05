3e91983b60a3773a5c5f547dfaf0fe422209f17c
Rellyso <rellysondouglas2015@gmail.com>
2021-05-04 21:30:30 -0300

    Refactoring: addded component logo

---
+++ src/components/Header/Logo.tsx
@@ -0,0 +1,18 @@
+import { Text } from "@chakra-ui/layout";
+
+export function Logo() {
+  return (
+    <Text
+      fontSize={["2xl", "3xl"]}
+      fontWeight="bold"
+      letterSpacing="tight"
+      w="64"
+    >
+      dashgo
+
+      <Text color="pink.500" ml="1" as="span">
+        .
+      </Text>
+    </Text>
+  )
+}
