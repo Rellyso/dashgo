3f8fa4f377070d2d291c0aebfd1457321d4b5373
Rellyso <rellysondouglas2015@gmail.com>
2021-05-05 21:49:54 -0300

    Added Links and anchors to navigate & show to user

--- src/components/Sidebar/NavLink.tsx
+++ src/components/Sidebar/NavLink.tsx
@@ -1,18 +1,21 @@
 import { LinkProps as ChakraLinkProps } from "@chakra-ui/react";
 import Icon from "@chakra-ui/icon";
-import { Link, Text } from "@chakra-ui/layout";
+import { Link as ChakraLink, Text } from "@chakra-ui/layout";
 import { ElementType } from "react";
-
+import { ActiveLink } from "../ActiveLink";
 interface NavLinkProps extends ChakraLinkProps {
   icon: ElementType;
   children: string;
+  href: string;
 }
 
-export function NavLink({ icon, children, ...rest }: NavLinkProps) {
+export function NavLink({ icon, children, href, ...rest }: NavLinkProps) {
   return (
-    <Link display="flex" align="center" {...rest}>
-      <Icon as={icon} fontSize="20" />
-      <Text ml="4" fontWeight="bold">{children}</Text>
-    </Link>
+    <ActiveLink href={href} passHref>
+      <ChakraLink display="flex" align="center" {...rest}>
+        <Icon as={icon} fontSize="20" />
+        <Text ml="4" fontWeight="bold">{children}</Text>
+      </ChakraLink>
+    </ActiveLink>
   )
 }
