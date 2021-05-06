819025450a9ffc3ea65b2f4944f7f6c6362492be
Rellyso <rellysondouglas2015@gmail.com>
2021-05-05 21:49:13 -0300

    Added Drawer menu on smaller screens

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,20 +1,30 @@
 import { Box } from "@chakra-ui/layout";
 import { useBreakpointValue } from "@chakra-ui/media-query";
-import { Drawer, DrawerOverlay } from "@chakra-ui/modal";
+import { Drawer, DrawerBody, DrawerCloseButton, DrawerContent, DrawerHeader, DrawerOverlay } from "@chakra-ui/modal";
+import { useSidebarDrawer } from "../../contexts/SidebarDrawerContext";
 import { SidebarNav } from "./SidebarNav";
 
 export function Sidebar() {
-  const isDrawerSidebar = useBreakpointValue({
+  const isDrawerSidebar = useBreakpointValue({ // verify if is floating sidebar
     base: true,
     lg: false,
   })
 
   if (isDrawerSidebar) {
-    <Drawer isOpen={true} placement="left" onClose={() => { }}>
-      <DrawerOverlay>
-
-      </DrawerOverlay>
-    </Drawer>
+    const { isOpen, onClose } = useSidebarDrawer();
+    return (
+      <Drawer isOpen={isOpen} placement="left" onClose={onClose}>
+        <DrawerOverlay>
+          <DrawerContent bg="gray.800" p="4">
+            <DrawerCloseButton mt="6" />
+            <DrawerHeader>Navegação</DrawerHeader>
+            <DrawerBody>
+              <SidebarNav />
+            </DrawerBody>
+          </DrawerContent>
+        </DrawerOverlay>
+      </Drawer>
+    )
   }
 
   return (
