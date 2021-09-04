3fa501f2f12ddcce35456236f50cbcd50b120c19
Rellyson Douglas <rellysondouglas2015@gmail.com>
2021-09-04 16:01:33 -0300

    updated to set working users creation

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -7,6 +7,10 @@ import { Sidebar } from "../../components/Sidebar";
 import { useForm, SubmitHandler } from 'react-hook-form'
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup'
+import { useMutation } from 'react-query'
+import { api } from "../../services/api";
+import { clientQuery } from "../../services/queryClient";
+import { useRouter } from "next/router";
 
 type CreateUserFormData = {
   name: string;
@@ -18,23 +22,40 @@ type CreateUserFormData = {
 const createUserFormSchema = yup.object().shape({
   name: yup.string().required('Nome obrigatório'),
   email: yup.string().required('E-mail obrigatório').email('E-email inválido'),
-  password: yup.string().required('Senha obrigatória').min(8, 'No mínimo 6 caracteres'),
+  password: yup.string().required('Senha obrigatória').min(6, 'No mínimo 6 caracteres'),
   password_confirmation: yup.string().oneOf([
     null, yup.ref('password')
   ], 'As senhas precisam ser iguais.'),
 })
 
 export default function CreateUser() {
+  const router = useRouter();
+
+  const createUser = useMutation(async (user: CreateUserFormData) => {
+    const response = await api.post('users', {
+      user: {
+        ...user,
+        created_at: new Date(),
+      }
+    })
+
+    return response.data.user;
+  }, {
+    onSuccess: () => {
+      clientQuery.invalidateQueries(['users', 1])
+    }
+  });
+
   const { register, handleSubmit, formState } = useForm({
     resolver: yupResolver(createUserFormSchema)
   })
 
   const { errors } = formState
 
-  const handleCreateUser: SubmitHandler<HTMLInputElement> = async (values) => {
-    await new Promise(resolve => setTimeout(resolve, 2000));
+  const handleCreateUser: SubmitHandler<CreateUserFormData> = async (values) => {
+    await createUser.mutateAsync(values);
 
-    console.log(values)
+    router.push('/users')
   }
 
   return (
